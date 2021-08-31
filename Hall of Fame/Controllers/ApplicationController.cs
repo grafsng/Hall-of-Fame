6cec6bbeaffbddb143aadfe4314128cbe50cb2ff
Чингис Чингис <gikipiki19@gmail.com>
2021-08-31 13:04:39 +0700

    Создание пустого API контроллера

---
+++ Hall of Fame/Controllers/ApplicationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hall_of_Fame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+    }
+}
