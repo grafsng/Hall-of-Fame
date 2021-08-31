5935ed6d5f0ad2c1a3f620d2a7aecce4f267d656
Чингис Чингис <gikipiki19@gmail.com>
2021-08-31 12:29:07 +0700

    Созданы модели

---
+++ Hall of Fame/Models/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hall_of_Fame.Models
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+
+        public byte Level { get; set; }
+
+        public long PersonId { get; set; }
+
+    }
+}
